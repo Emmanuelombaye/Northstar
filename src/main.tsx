62a0149cf54abd68a49e975f8da19671831957ff
James Sio <donfilmproductions@gmail.com>
2026-06-16 17:47:36 -0700

    fix: import missing marketing CSS files in React app entry

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,15 @@ import "../css/shop.css";
 import "../css/pharm.css";
 import "../css/checkout-intake.css";
 
+// Marketing Pages CSS
+import "../css/about.css";
+import "../css/explore-treatments.css";
+import "../css/faq.css";
+import "../css/how-it-works.css";
+import "../css/membership.css";
+import "../css/resources.css";
+import "../css/treatments.css";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
